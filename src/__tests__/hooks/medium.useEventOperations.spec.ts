826b62b7e673c20e1e701833897f880dd5c172d5
Harry <harry@bzznbyd.com>
2024-11-06 03:25:26 +0900

    test: 초기 데이터 가져오기, 업데이트 및 추가 테스트 진행중

--- src/__tests__/hooks/medium.useEventOperations.spec.ts
+++ src/__tests__/hooks/medium.useEventOperations.spec.ts
@@ -1,7 +1,8 @@
-import { act, renderHook } from '@testing-library/react';
+import { act, renderHook, waitFor } from '@testing-library/react';
 import { http, HttpResponse } from 'msw';
 
 import {
+  resetMockEvents,
   setupMockHandlerCreation,
   setupMockHandlerDeletion,
   setupMockHandlerUpdating,
@@ -10,9 +11,56 @@ import { useEventOperations } from '../../hooks/useEventOperations.ts';
 import { server } from '../../setupTests.ts';
 import { Event } from '../../types.ts';
 
-it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다', async () => {});
+afterEach(() => {
+  server.resetHandlers();
+  resetMockEvents();
+});
 
-it('정의된 이벤트 정보를 기준으로 적절하게 저장이 된다', async () => {});
+it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다', async () => {
+  const { result } = renderHook(() => useEventOperations(false));
+  await waitFor(() => {
+    expect(result.current.events).toEqual([
+      {
+        id: '1',
+        title: '기존 회의',
+        date: '2024-10-15',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+    ]);
+  });
+});
+
+it('정의된 이벤트 정보를 기준으로 적절하게 저장이 된다', async () => {
+  setupMockHandlerCreation();
+
+  const newEvent: Event = {
+    id: '1234',
+    title: '신규 이벤트',
+    date: '2024-11-06',
+    startTime: '10:00',
+    endTime: '11:00',
+    description: '신규 이벤트 설명',
+    location: '회의실 A',
+    category: '업무',
+    repeat: { type: 'none', interval: 0 },
+    notificationTime: 10,
+  };
+
+  const { result } = renderHook(() => useEventOperations(false));
+
+  await act(async () => {
+    await result.current.saveEvent(newEvent);
+  });
+  console.log('!@$', result.current);
+
+  expect(result.current.events).toContainEqual(newEvent);
+});
 
 it("새로 정의된 'title', 'endTime' 기준으로 적절하게 일정이 업데이트 된다", async () => {});
 
